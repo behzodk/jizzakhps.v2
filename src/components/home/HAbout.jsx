c3e7bdcd06e0abe9f0d86e5552087bc8622aecf7
Behzod <89260874+behzodk@users.noreply.github.com>
2023-11-14 20:18:48 +0500

    Update HAbout.jsx

--- src/components/home/HAbout.jsx
+++ src/components/home/HAbout.jsx
@@ -1,6 +1,6 @@
 import React from "react"
-import OnlineCourses from "../allcourses/OnlineCourses"
-import Heading from "../common/heading/Heading"
+// import OnlineCourses from "../allcourses/OnlineCourses"
+// import Heading from "../common/heading/Heading"
 import "../allcourses/courses.css"
 import { coursesCard } from "../../dummydata"
 
